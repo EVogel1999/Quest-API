755a821e28a4fbf2f04682e9e93c727abe21a06e
Emily Vogel <eav990@gmail.com>
2019-09-30 20:36:54 -0400

    Flesh out schema

--- src/main/java/com/example/Quest/Demo/Database/Schema/Hero.java
+++ src/main/java/com/example/Quest/Demo/Database/Schema/Hero.java
@@ -1,4 +1,43 @@
 package com.example.Quest.Demo.Database.Schema;
 
 public class Hero {
+    private String _name;
+    private String _class;
+    private int _level;
+
+    public Hero() {
+        _name = "";
+        _class = "";
+        _level = -1;
+    }
+
+    public Hero(String _name, String _class, int _level) {
+        this._name = _name;
+        this._class = _class;
+        this._level = _level;
+    }
+
+    public String get_name() {
+        return _name;
+    }
+
+    public void set_name(String _name) {
+        this._name = _name;
+    }
+
+    public String get_class() {
+        return _class;
+    }
+
+    public void set_class(String _class) {
+        this._class = _class;
+    }
+
+    public int get_level() {
+        return _level;
+    }
+
+    public void set_level(int _level) {
+        this._level = _level;
+    }
 }
--- src/main/java/com/example/Quest/Demo/Database/Schema/Quest.java
+++ src/main/java/com/example/Quest/Demo/Database/Schema/Quest.java
@@ -1,4 +1,67 @@
 package com.example.Quest.Demo.Database.Schema;
 
+import java.util.Date;
+
 public class Quest {
+    private String _name;
+    private String _description;
+    private int _reward;
+    private boolean _completed;
+    private Date _expires;
+
+    public Quest() {
+        _name = "";
+        _description = "";
+        _reward = -1;
+        _completed = false;
+        _expires = null;
+    }
+
+    public Quest(String _name, String _description, int _reward, boolean _completed, Date _expires) {
+        this._name = _name;
+        this._description = _description;
+        this._reward = _reward;
+        this._completed = _completed;
+        this._expires = _expires;
+    }
+
+    public String get_name() {
+        return _name;
+    }
+
+    public void set_name(String _name) {
+        this._name = _name;
+    }
+
+    public String get_description() {
+        return _description;
+    }
+
+    public void set_description(String _description) {
+        this._description = _description;
+    }
+
+    public int get_reward() {
+        return _reward;
+    }
+
+    public void set_reward(int _reward) {
+        this._reward = _reward;
+    }
+
+    public boolean get_completed() {
+        return _completed;
+    }
+
+    public void set_completed(boolean _completed) {
+        this._completed = _completed;
+    }
+
+    public Date get_expires() {
+        return _expires;
+    }
+
+    public void set_expires(Date _expires) {
+        this._expires = _expires;
+    }
 }
