e278829b0aba9a208920a604ea74732f6729931e
Emily Vogel <eav990@gmail.com>
2019-10-03 10:20:20 -0400

    Add CORS config

--- src/main/java/com/example/Quest/Demo/QuestDemoApplication.java
+++ src/main/java/com/example/Quest/Demo/QuestDemoApplication.java
@@ -2,6 +2,10 @@ package com.example.Quest.Demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class QuestDemoApplication {
@@ -10,4 +14,15 @@ public class QuestDemoApplication {
 		SpringApplication.run(QuestDemoApplication.class, args);
 	}
 
+	/**
+	 * CORS configuration for when connecting this API to a client
+	 */
+	@EnableWebMvc
+	@Configuration
+	public class WebConfig implements WebMvcConfigurer {
+		@Override
+		public void addCorsMappings(CorsRegistry registry) {
+			registry.addMapping("/**");
+		}
+	}
 }
